3dc93fc366a3fa8fd7243bfa5338fadeb104f24f
S.m Mustakim Bellah <59763884+mustakimbellah@users.noreply.github.com>
2022-10-11 01:56:49 +0600

    nav section ready

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
     return (
-        <nav>
-            <Link to='/'>Home</Link>
-            <Link to='/topics'>Topics</Link>
-            <Link to='/statistics'>Statistics</Link>
-            <Link to='/blog'>Blog</Link>
+        <nav className='nav-container'>
+            <h1>Knowledge Test</h1>
+            <div>
+                <Link to='/'>Home</Link>
+                <Link to='/topics'>Topics</Link>
+                <Link to='/statistics'>Statistics</Link>
+                <Link to='/blog'>Blog</Link>
+            </div>
         </nav>
     );
 };
