88c5fd4faf50953099bcbd1864e6abbe28d1adbc
S.m Mustakim Bellah <59763884+mustakimbellah@users.noreply.github.com>
2022-10-11 00:58:36 +0600

    nav route added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-//import logo from './logo.svg';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Main from './layouts/Main';
+import Home from './components/Home/Home';
+import Blog from './components/Blog/Blog';
+import Statistics from './components/Statistics/Statistics';
+import Topics from './components/Topics/Topics';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        },
+        {
+          path: '/blog',
+          element: <Blog></Blog>
+        },
+
+        {
+          path: '/statistics',
+          element: <Statistics></Statistics>
+        },
+
+        {
+          path: '/topics',
+          element: <Topics></Topics>
+        },
+      ]
+    }
+  ])
   return (
     <div className="App">
-
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
